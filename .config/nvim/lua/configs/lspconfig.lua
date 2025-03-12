9d25662a04deef326c6d2a4e18f9858a8d0c76a6
Tim Lauridsen <tla@rasmil.dk>
2025-03-12 19:36:47 +0100

    cleanup LSP config

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,28 +1,22 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
-require("lspconfig").html.setup {}
-require("lspconfig").clangd.setup {}
-require("lspconfig").markdown_oxide.setup {}
-require("lspconfig").autotools_ls.setup {}
 
-local lspconfig = require "lspconfig"
+local configs = require "nvchad.configs.lspconfig"
 
--- EXAMPLE
-local servers = { "html", "cssls", "ruff", "autotools_ls" }
-local nvlsp = require "nvchad.configs.lspconfig"
+local servers = {
+  html = {},
+  awk_ls = {},
+  bashls = {},
+  ruff = {},
+  cssls = {},
+  autotools_ls = {},
+  markdown_oxide = {},
+  clangd = {},
+}
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+for name, opts in pairs(servers) do
+  opts.on_init = configs.on_init
+  opts.on_attach = configs.on_attach
+  opts.capabilities = configs.capabilities
+  require("lspconfig")[name].setup(opts)
 end
-
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
