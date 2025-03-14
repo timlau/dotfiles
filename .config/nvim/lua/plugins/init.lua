e5cb76b78c3132bf6c756521a08ce04ceb30c706
Tim Lauridsen <tla@rasmil.dk>
2025-03-14 17:59:22 +0100

    add github copilot plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -85,4 +85,27 @@ return {
       { "<leader>dt", "<cmd>Trouble todo toggle<CR>", desc = "Open todos in trouble" },
     },
   },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "BufReadPost",
+    opts = {
+      suggestion = {
+        enabled = not vim.g.ai_cmp,
+        auto_trigger = true,
+        hide_during_completion = vim.g.ai_cmp,
+        keymap = {
+          accept = false, -- handled by nvim-cmp / blink.cmp
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
 }
