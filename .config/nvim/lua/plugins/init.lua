4ddbdff097d423f326fffc9ca2fbd7087022a834
Tim Lauridsen <tla@rasmil.dk>
2025-03-14 18:33:45 +0100

    split plugins into separate lua files

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -12,7 +12,6 @@ return {
       require "configs.lspconfig"
     end,
   },
-
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
@@ -22,89 +21,6 @@ return {
         "vimdoc",
         "html",
         "css",
-      },
-    },
-  },
-  {
-    "kdheepak/lazygit.nvim",
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
-    },
-  },
-  {
-    "mfussenegger/nvim-lint",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local lint = require "lint"
-
-      lint.linters_by_ft = {
-        python = { "ruff" },
-        lua = { "luacheck" },
-        yaml = { "ansible_lint" },
-      }
-
-      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
-      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-        group = lint_augroup,
-        callback = function()
-          lint.try_lint()
-        end,
-      })
-
-      vim.keymap.set("n", "<leader>lc", function()
-        lint.try_lint()
-      end, { desc = "Trigger linting for current file" })
-    end,
-  },
-  {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
-    opts = {
-      focus = true,
-    },
-    cmd = "Trouble",
-    keys = {
-      { "<leader>dw", "<cmd>Trouble diagnostics toggle<CR>", desc = "Open trouble workspace diagnostics" },
-      { "<leader>dd", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Open trouble document diagnostics" },
-      { "<leader>dq", "<cmd>Trouble quickfix toggle<CR>", desc = "Open trouble quickfix list" },
-      { "<leader>dl", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
-      { "<leader>dt", "<cmd>Trouble todo toggle<CR>", desc = "Open todos in trouble" },
-    },
-  },
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    event = "BufReadPost",
-    opts = {
-      suggestion = {
-        enabled = not vim.g.ai_cmp,
-        auto_trigger = true,
-        hide_during_completion = vim.g.ai_cmp,
-        keymap = {
-          accept = false, -- handled by nvim-cmp / blink.cmp
-          next = "<M-]>",
-          prev = "<M-[>",
-        },
-      },
-      panel = { enabled = false },
-      filetypes = {
-        markdown = true,
-        help = true,
       },
     },
   },
