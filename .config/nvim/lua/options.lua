d510773a4e1e8f1a170a8a97be913206fb9c09e8
Tim Lauridsen <tla@rasmil.dk>
2025-07-31 18:24:14 +0200

    replace neovim config files with lazyvim

--- .config/nvim/lua/options.lua
+++
@@ -1,8 +0,0 @@
-require "nvchad.options"
-
--- add yours here!
-vim.g.vscode_snippets_path = "/usr/share/code/resources/app/extensions/*/snippets"
-
-local o = vim.o
-o.cursorlineopt = "both" -- to enable cursorline!
-o.relativenumber = true
