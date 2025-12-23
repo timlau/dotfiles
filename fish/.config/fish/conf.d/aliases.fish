09fdcef777de4ad43cf28593e077508d57b382f2
Tim Lauridsen <tla@rasmil.dk>
2025-12-23 14:25:12 +0100

    update fish aliases

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -1,28 +1,28 @@
 # This file is managed by Ansible. Do not edit manually.
 
 # Define aliases (which are functions in fish)
-function ll --wraps 'ls' --description 'alias ll=ls -lhA'
+function ll --wraps ls --description 'alias ll=ls -lhA'
     ls -lhA $argv
 end
 
-function gco --wraps 'git' --description 'alias gco=git checkout'
+function gco --wraps git --description 'alias gco=git checkout'
     git checkout $argv
 end
 
-function cat --wraps 'bat' --description 'alias cat=bat'
+function cat --wraps bat --description 'alias cat=bat'
     bat $argv
 end
 
-function lg --wraps 'lazygit' --description 'alias lg=lazygit'
+function lg --wraps lazygit --description 'alias lg=lazygit'
     lazygit $argv
 end
 
-function vi --wraps 'nvim' --description 'alias vi=nvim'
+function vi --wraps nvim --description 'alias vi=nvim'
     nvim $argv
 end
 
 function upd --description 'update dotfiles to/from github'
-    cd ~/udv/github/dotfiles
+    cd ~/dotfiles
     git commit -a -m "update config"
     git pull --rebase origin main
     git push
