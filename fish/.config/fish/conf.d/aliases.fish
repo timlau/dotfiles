6e235b6e8929dcf5bc1ed9fa50f521f21bdc190a
Tim Lauridsen <tla@rasmil.dk>
2025-12-26 18:02:11 +0100

    + added y alias to yazi

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -29,3 +29,12 @@ function upd --description 'update dotfiles to/from github'
     git push
     cd -
 end
+
+function y --description 'yazi filemanager change to cwd on exit'
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
