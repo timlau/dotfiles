25339e3d4026e9d3df6860a5151c7b0c58021691
Tim Lauridsen <tla@rasmil.dk>
2025-12-30 11:21:21 +0100

    + nvim: map Ctrl-Z/R to undo/redo

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- map <C-z> to undo in normal mode
+vim.keymap.set("n", "<C-z>", ":undo<CR>", { desc = "Undo" })
+
+-- Optional: map <C-r> to redo in normal mode
+vim.keymap.set("n", "<C-r>", ":redo<CR>", { desc = "Redo" })
